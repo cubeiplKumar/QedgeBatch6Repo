eb6e5a6d884800ec3e4765e67cacd7181952e26b
cubeiplkumar <emailanilkumarm@gmail.com>
2024-04-23 09:56:41 +0530

    Feature203 changes in Application.java file

--- Application.java
+++ Application.java
@@ -60,4 +60,24 @@ class Test {
         System.out.println(
             "In method m2 came from method m1");
     }
+	#feature203 changes by Developer Ana Oct2023 for CitiBank ApplicationException
+	// create a method
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+    // return value
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    
+    int num1 = 25;
+    int num2 = 15;
+
+    // create an object of Main
+    Main obj = new Main();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+  }
+}
 }
